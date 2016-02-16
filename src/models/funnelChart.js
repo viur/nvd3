53198d2d5694267fcdfc2c29281297b5e4171a64
Pedro Almeida <pedro@viur.pt>
2016-02-16 22:31:45 +0000

    added funnel chart

---
+++ src/models/funnelChart.js
@@ -0,0 +1,321 @@
+nv.models.funnelChart = function () {
+    "use strict";
+
+    //============================================================
+    // Public Variables with Default Settings
+    //------------------------------------------------------------
+
+    var funnel = nv.models.funnel()
+        , xAxis = nv.models.axis()
+        , yAxis = nv.models.axis()
+        , tooltip = nv.models.tooltip()
+        ;
+
+    var margin = {top: 15, right: 10, bottom: 50, left: 60}
+        , width = null
+        , height = null
+        , color = nv.utils.getColor()
+        , showXAxis = true
+        , showYAxis = true
+        , rightAlignYAxis = false
+        , staggerLabels = false
+        , x
+        , y
+        , noData = null
+        , dispatch = d3.dispatch('beforeUpdate', 'renderEnd')
+        , duration = 250
+        ;
+
+    xAxis
+        .orient('bottom')
+        .showMaxMin(false)
+        .tickFormat(function (d) {
+            return d
+        })
+    ;
+    yAxis
+        .orient((rightAlignYAxis) ? 'right' : 'left')
+        .tickFormat(d3.format(',.1f'))
+    ;
+
+    tooltip
+        .duration(0)
+        .headerEnabled(false)
+        .valueFormatter(function (d, i) {
+            return yAxis.tickFormat()(d, i);
+        })
+        .keyFormatter(function (d, i) {
+            return xAxis.tickFormat()(d, i);
+        });
+
+    //============================================================
+    // Private Variables
+    //------------------------------------------------------------
+
+    var renderWatch = nv.utils.renderWatch(dispatch, duration);
+
+    function chart(selection) {
+        renderWatch.reset();
+        renderWatch.models(funnel);
+        if (showXAxis) renderWatch.models(xAxis);
+        if (showYAxis) renderWatch.models(yAxis);
+
+        selection.each(function (data) {
+            var container = d3.select(this),
+                that = this;
+            nv.utils.initSVG(container);
+            var availableWidth = nv.utils.availableWidth(width, container, margin),
+                availableHeight = nv.utils.availableHeight(height, container, margin);
+
+            chart.update = function () {
+                dispatch.beforeUpdate();
+                container.transition().duration(duration).call(chart);
+            };
+            chart.container = this;
+
+            // Display No Data message if there's nothing to show.
+            if (!data || !data.length || !data.filter(function (d) {
+                    return d.values.length
+                }).length) {
+                nv.utils.noData(chart, container);
+
+                //FIX Clean previous chart
+                container.selectAll('.nv-wrap').remove();
+
+                return chart;
+            } else {
+                container.selectAll('.nv-noData').remove();
+            }
+
+            // Setup Scales
+            x = funnel.xScale();
+            y = funnel.yScale().clamp(true);
+
+            // Setup containers and skeleton of chart
+            var wrap = container.selectAll('g.nv-wrap.nv-discreteBarWithAxes').data([data]);
+            var gEnter = wrap.enter().append('g').attr('class', 'nvd3 nv-wrap nv-discreteBarWithAxes').append('g');
+            var defsEnter = gEnter.append('defs');
+            var g = wrap.select('g');
+
+            gEnter.append('g').attr('class', 'nv-x nv-axis');
+            gEnter.append('g').attr('class', 'nv-y nv-axis')
+                .append('g').attr('class', 'nv-zeroLine')
+                .append('line');
+
+            gEnter.append('g').attr('class', 'nv-barsWrap');
+
+            g.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+            if (rightAlignYAxis) {
+                g.select(".nv-y.nv-axis")
+                    .attr("transform", "translate(" + availableWidth + ",0)");
+            }
+
+            // Main Chart Component(s)
+            funnel
+                .width(availableWidth)
+                .height(availableHeight);
+
+            var barsWrap = g.select('.nv-barsWrap')
+                .datum(data.filter(function (d) {
+                    return !d.disabled
+                }));
+
+            barsWrap.transition().call(funnel);
+
+
+            defsEnter.append('clipPath')
+                .attr('id', 'nv-x-label-clip-' + funnel.id())
+                .append('rect');
+
+            g.select('#nv-x-label-clip-' + funnel.id() + ' rect')
+                .attr('width', x.rangeBand() * (staggerLabels ? 2 : 1))
+                .attr('height', 16)
+                .attr('x', -x.rangeBand() / (staggerLabels ? 1 : 2 ));
+
+            // Setup Axes
+            if (showXAxis) {
+                xAxis
+                    .scale(x)
+                    ._ticks(nv.utils.calcTicksX(availableWidth / 100, data))
+                    .tickSize(-availableHeight, 0);
+
+                g.select('.nv-x.nv-axis')
+                    .attr('transform', 'translate(0,' + (y.range()[0] + ((funnel.showValues() && y.domain()[0] < 0) ? 16 : 0)) + ')');
+                g.select('.nv-x.nv-axis').call(xAxis);
+
+                var xTicks = g.select('.nv-x.nv-axis').selectAll('g');
+                if (staggerLabels) {
+                    xTicks
+                        .selectAll('text')
+                        .attr('transform', function (d, i, j) {
+                            return 'translate(0,' + (j % 2 == 0 ? '5' : '17') + ')'
+                        })
+                }
+            }
+
+            if (showYAxis) {
+                yAxis
+                    .scale(y)
+                    ._ticks(nv.utils.calcTicksY(availableHeight / 36, data))
+                    .tickSize(-availableWidth, 0);
+
+                g.select('.nv-y.nv-axis').call(yAxis);
+            }
+
+            // Zero line
+            g.select(".nv-zeroLine line")
+                .attr("x1", 0)
+                .attr("x2", availableWidth)
+                .attr("y1", y(0))
+                .attr("y2", y(0))
+            ;
+        });
+
+        renderWatch.renderEnd('discreteBar chart immediate');
+        return chart;
+    }
+
+    //============================================================
+    // Event Handling/Dispatching (out of chart's scope)
+    //------------------------------------------------------------
+
+    funnel.dispatch.on('elementMouseover.tooltip', function (evt) {
+        evt['series'] = {
+            key: chart.x()(evt.data),
+            value: chart.y()(evt.data),
+            color: evt.color
+        };
+        tooltip.data(evt).hidden(false);
+    });
+
+    funnel.dispatch.on('elementMouseout.tooltip', function (evt) {
+        tooltip.hidden(true);
+    });
+
+    funnel.dispatch.on('elementMousemove.tooltip', function (evt) {
+        tooltip.position({top: d3.event.pageY, left: d3.event.pageX})();
+    });
+
+    //============================================================
+    // Expose Public Variables
+    //------------------------------------------------------------
+
+    chart.dispatch = dispatch;
+    chart.funnel = funnel;
+    chart.xAxis = xAxis;
+    chart.yAxis = yAxis;
+    chart.tooltip = tooltip;
+
+    chart.options = nv.utils.optionsFunc.bind(chart);
+
+    chart._options = Object.create({}, {
+        // simple options, just get/set the necessary values
+        width: {
+            get: function () {
+                return width;
+            }, set: function (_) {
+                width = _;
+            }
+        },
+        height: {
+            get: function () {
+                return height;
+            }, set: function (_) {
+                height = _;
+            }
+        },
+        staggerLabels: {
+            get: function () {
+                return staggerLabels;
+            }, set: function (_) {
+                staggerLabels = _;
+            }
+        },
+        showXAxis: {
+            get: function () {
+                return showXAxis;
+            }, set: function (_) {
+                showXAxis = _;
+            }
+        },
+        showYAxis: {
+            get: function () {
+                return showYAxis;
+            }, set: function (_) {
+                showYAxis = _;
+            }
+        },
+        noData: {
+            get: function () {
+                return noData;
+            }, set: function (_) {
+                noData = _;
+            }
+        },
+
+        // deprecated options
+        tooltips: {
+            get: function () {
+                return tooltip.enabled();
+            }, set: function (_) {
+                // deprecated after 1.7.1
+                nv.deprecated('tooltips', 'use chart.tooltip.enabled() instead');
+                tooltip.enabled(!!_);
+            }
+        },
+        tooltipContent: {
+            get: function () {
+                return tooltip.contentGenerator();
+            }, set: function (_) {
+                // deprecated after 1.7.1
+                nv.deprecated('tooltipContent', 'use chart.tooltip.contentGenerator() instead');
+                tooltip.contentGenerator(_);
+            }
+        },
+
+        // options that require extra logic in the setter
+        margin: {
+            get: function () {
+                return margin;
+            }, set: function (_) {
+                margin.top = _.top !== undefined ? _.top : margin.top;
+                margin.right = _.right !== undefined ? _.right : margin.right;
+                margin.bottom = _.bottom !== undefined ? _.bottom : margin.bottom;
+                margin.left = _.left !== undefined ? _.left : margin.left;
+            }
+        },
+        duration: {
+            get: function () {
+                return duration;
+            }, set: function (_) {
+                duration = _;
+                renderWatch.reset(duration);
+                funnel.duration(duration);
+                xAxis.duration(duration);
+                yAxis.duration(duration);
+            }
+        },
+        color: {
+            get: function () {
+                return color;
+            }, set: function (_) {
+                color = nv.utils.getColor(_);
+                funnel.color(color);
+            }
+        },
+        rightAlignYAxis: {
+            get: function () {
+                return rightAlignYAxis;
+            }, set: function (_) {
+                rightAlignYAxis = _;
+                yAxis.orient((_) ? 'right' : 'left');
+            }
+        }
+    });
+
+    nv.utils.inheritOptions(chart, funnel);
+    nv.utils.initOptions(chart);
+
+    return chart;
+};
