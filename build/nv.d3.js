f5f4497614e701fe832d4fb28ca9f09969e91f86
Pedro Almeida <pedro@viurdata.com>
2018-06-20 10:59:31 +0100

    build

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -1,4 +1,4 @@
-/* nvd3 version 1.8.3 (https://github.com/novus/nvd3) 2018-06-19 */
+/* nvd3 version 1.8.3 (https://github.com/novus/nvd3) 2018-06-20 */
 (function(){
 
 // set up main nv object
@@ -10974,7 +10974,7 @@ nv.models.multiBarHorizontalChart = function() {
                     var sampleText = wrap.append("text").style('opacity', 0).text('Sample');
                     var sampleTextHeight = sampleText.node().getBBox().height;
 
-                    var availableLeft = margin.left - 15;
+                    var availableLeft = margin.left - 20;
 
                     if(chart.xAxis.axisLabel() !== null && chart.xAxis.axisLabel() !== ""){
                         availableLeft = margin.left - chart.xAxis.axisLabelDistance() - (sampleTextHeight * 1.50); //36 is set by nvd3 in axis
