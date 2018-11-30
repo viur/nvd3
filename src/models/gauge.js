37a0dfba961b35b75f9a74bcc0f3e4a7b3098cc9
Pedro Almeida <pedro@viurdata.com>
2018-11-30 13:17:56 +0000

    fix gauge

--- src/models/gauge.js
+++ src/models/gauge.js
@@ -421,7 +421,7 @@ nv.models.gauge = function () {
                         }else{
                             switch (valueType) {
                                 case 'absolute':
-                                    return valueFormat(value);
+                                    return valueFormat(max);
                                 case 'percent':
                                     return d3.format('%')(1);
                             }
